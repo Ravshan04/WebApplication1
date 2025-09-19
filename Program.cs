2fa1e0bf61d0bbc47535f107d5fd43658ee42f4a
K1ga <r.ean.e.s.c.aekordi@gmail.com>
2025-09-19 16:50:50 +0500

    adad

--- Program.cs
+++ Program.cs
@@ -2,10 +2,7 @@ namespace WebApplication1
 {
     public class Program
     {
-        /// <summary>
-        /// double click to run
-        /// </summary>
-        /// <param name="args"></param>
+    
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
